b2a8c7c44ffed3e44810e496334d86ed547f02d2
heetpatel1200 <heetpatel1200@gmail.com>
2026-01-13 18:00:43 +0530

    Add files via upload

---
+++ p5.java
@@ -0,0 +1,50 @@
+Simulate a simple ATM or cashier. Given an integer amount to be dispensed (e.g., 787),
+Calculate and display the minimum number of currency notes of denominations 100, 50, 10,
+5, 2, and 1 that would be given to the user.
+
+
+import java.util.Scanner;
+public class p5{
+	public static void main(String args[]){
+		
+		int amount,d;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the Amount:");
+		amount=sc.nextInt();
+
+		d=amount/100;
+		amount%=100;
+
+		System.out.println("Number of 100 notes:"+d);
+
+		d=amount/50;
+		amount%=50;
+
+		System.out.println("Number of 50 notes:"+d);
+
+		d=amount/10;
+		amount%=10;
+
+		System.out.println("Number of 10 notes:"+d);
+		
+		d=amount/5;
+		amount%=5;
+
+		System.out.println("Number of 5 notes:"+d);
+
+		d=amount/2;
+		amount%=2;
+
+		System.out.println("Number of 2 notes:"+d);
+
+		d=amount/1;
+
+		System.out.println("Number of 1 notes:"+d);
+
+
+		sc.close();
+		
+	}
+}
---
+++ p6.java
@@ -0,0 +1,28 @@
+Write a Java program that accepts a five-digit integer from the keyboard. Your program
+should then create a new number by adding one to each digit of the input number. For
+example, if the input is 12391, the output should be 23402 (note: 9+1=10, so it becomes 0
+with a carry)
+
+import java.util.Scanner;
+class p6 {
+   public static void main(String[] args) {
+ 	Scanner sc = new Scanner(System.in);
+ 	System.out.print("Enter a five-digit number: ");
+ 
+int num = sc.nextInt();
+ int originalNum = num;
+ int result = 0;
+ int place = 1;  
+
+ for (int i = 0; i < 5; i++) {
+ 	int digit = num % 10; 
+ 	digit = (digit + 1) % 10;  
+ 	result = digit * place + result; 
+ 	place *= 10; 
+ 	num = num / 10; 
+ }
+ 	System.out.println("Original number: " + originalNum);
+ 	System.out.println("New number: " + result);
+ 	sc.close();
+ }
+}
